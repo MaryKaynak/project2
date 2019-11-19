5a1a000c61f5dd98b6402c61d4087838830e61b7
MaryKaynak <56208212+MaryKaynak@users.noreply.github.com>
2019-11-18 21:43:28 -0500

    test

--- src/Counties.java
+++ src/Counties.java
@@ -1,6 +1,7 @@
 import org.junit.Test;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.Iterator;
 
@@ -11,7 +12,9 @@ public class Counties {
      * method should take a tree number(int) and return smallest number(int)
      * Write a Java method to find the smallest number among three numbers.
      */
-
+    public int smallestNumber(int a,int b,int c){
+        return Math.min(Math.min( a, b), c);
+    }
 
     /** Create a method
      * method name is middleCharacter    <-------   METHOD NAME SHOULD BE AS THIS
@@ -19,7 +22,14 @@ public class Counties {
      * Write a Java method to display the middle character of a string
      * if the string length is even number return 0
      */
-
+           public char middleCharater(String a){
+               char [] arr = a.toCharArray();
+               if (a.length() % 2 == 0) {
+                   return '0';
+               }
+               char x = arr[arr.length / 2];
+               return x;
+           }
 
     /** Create a method
      * method name is dayOfTheWeek    <-------   METHOD NAME SHOULD BE AS THIS
@@ -28,16 +38,44 @@ public class Counties {
      * if the number is 2 return tuesday
      * if the number bigger then 7 then return -->> this is not a expected input
      */
+       public String dayoftheWeek(int a) {
+           if (a == 1) {
+               return "Monday";
+           } else if (a == 2) {
+               return "Tuesday";
+           } else if (a == 3) {
+               return "Wednesday";
+           } else if (a == 4) {
+               return "Thursday";
+           } else if (a == 5) {
+               return "Friday";
+           } else if (a == 6) {
+               return "Saturday";
+           } else if (a == 7) {
+               return "Sunday";
+           } else
+               return "this is not a expected input";
 
 
-    /**
-     * if CountThis String in the myCounties ArrayList then return how many of CountThis string in the myCounties
-     * if CountThis is not in the myCountries then return -1
-     */
+       }
+
+           /**
+            * if CountThis String in the myCounties ArrayList then return how many of CountThis string in the myCounties
+            * if CountThis is not in the myCountries then return -1
+            */
     public int countCountry(ArrayList<String> myCountries, String CountThis) {
-
+        if (!myCountries.contains(CountThis)) {
+            return -1;
+        }
         int result = 0;
         // START THE CODE FROM HERE
+
+        for (String a : myCountries) {
+            if (a.equals(CountThis)) {
+                result++;
+            }
+        }
+
 
 
         // CODE END HERE
@@ -51,6 +89,7 @@ public class Counties {
      */
     public ArrayList<String> sortArrayList(ArrayList<String> myCountries) {
 
+        Collections.sort(myCountries);
 
         return myCountries;
     }
@@ -62,29 +101,46 @@ public class Counties {
      */
     public ArrayList<String> reverseTheList(ArrayList<String> myCountries) {
         ArrayList<String> reverse = new ArrayList<>();
+        {
 //       start writing the code here
 
+            Collections.reverse(myCountries);
+            for (String x : myCountries) {
+                reverse.add(x);
+            }
 
 //        ends here
-        return reverse;
-    }
+            return reverse;
+        }
 
-    /**
-     * if first array contains second array then result is true if not then false
-     * Example1:
-     * first array =====>>> China Russia USA Mexico
-     * Second array =====>>>>Mexico Russia USA China
-     * result true
-     * Example2:
-     * first array =====>>> China USA Mexico
-     * Second array =====>>>>Mexico Russia USA China
-     * return false
-     */
+        /**
+         * if first array contains second array then result is true if not then false
+         * Example1:
+         * first array =====>>> China Russia USA Mexico
+         * Second array =====>>>>Mexico Russia USA China
+         * result true
+         * Example2:
+         * first array =====>>> China USA Mexico
+         * Second array =====>>>>Mexico Russia USA China
+         * return false
+         */
+    }
     public boolean isFirstArrayListContainsSecondArrayList(ArrayList<String> firstArray, ArrayList<String> SecondAray) {
 
+            boolean a = false;
+            for (String x: firstArray) {
+                for (String y: SecondAray) {
+                    if (x == y) {
+                        a = true;
+                    }
+                    else
+                        a = false;
+                }
+            }
+           return a;
+        }
 
-        return false;
-    }
+
 
     /**
      * if ArrayList myCountries have a firstCountry then replace it with a secondCountry
@@ -94,9 +150,15 @@ public class Counties {
      * result should be Brazil Canada Mexico Canada
      */
     public ArrayList<String> replaceTheCountry(ArrayList<String> myCountries, String firstCountry, String SecondCounry) {
-
-
+        for (String x: myCountries) {
+            if (x.contains(firstCountry)) {
+                int a = myCountries.indexOf(firstCountry);
+                myCountries.set(a,SecondCounry);
+            }
+        }
         return myCountries;
     }
 
-}
+    }
+
+
